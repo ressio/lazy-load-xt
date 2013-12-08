67f5f16cb50d95cdc248491275fa8bc4760175e0
Denis Ryabov <denis@mobilejoomla.com>
2013-12-08 23:54:04 +0400

    ver 0.8.2

--- dist/jquery.lazyloadxt.extra.js
+++ dist/jquery.lazyloadxt.extra.js
@@ -253,3 +253,27 @@
     });
 
 }(window.jQuery || window.Zepto, window));
+
+
+(function ($) {
+$.lazyLoadXT.selector = 'img,video,iframe[data-src]';
+
+    $(document).on('lazyshow', 'video', function () {
+        var $this = $(this);
+        $this
+            .attr('poster', $this.attr('data-poster'))
+            .removeAttr('data-poster')
+            .children()
+            .each(function () {
+                if (/source|track/i.test(this.tagName)) {
+                    var $child = $(this);
+                    $child
+                        .attr('src', $child.attr('data-src'))
+                        .removeAttr('data-src');
+                }
+            });
+        // reload video
+        this.load();
+    });
+
+}(window.jQuery || window.Zepto));

02d7150333cdb384e0e19e93407775fafc5d9224
Denis Ryabov <denis@mobilejoomla.com>
2013-12-09 15:27:48 +0400

    load all elements before print

---
+++ src/jquery.lazyloadxt.print.js
@@ -0,0 +1,19 @@
+/*jslint browser:true */
+/*jshint browser:true, jquery:true */
+
+(function ($, window) {
+    'use strict';
+
+    $.lazyLoadXT.forceEvent = 'beforeprint';
+
+    if (window.matchMedia) {
+        window
+            .matchMedia('print')
+            .addListener(function (mql) {
+                if (mql.matches) {
+                    $(window).trigger('beforeprint');
+                }
+            });
+    }
+
+}(window.jQuery || window.Zepto, window));

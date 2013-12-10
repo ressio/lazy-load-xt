572daaba9b9c513bab8e02c1aba767d4241863b1
Denis Ryabov <denis@mobilejoomla.com>
2013-12-10 15:22:42 +0400

    example of autoload of all images after a delay

---
+++ src/jquery.lazyloadxt.autoload.js
@@ -0,0 +1,16 @@
+/*jslint browser:true */
+/*jshint browser:true, jquery:true */
+
+(function ($) {
+    'use strict';
+
+    $.lazyLoadXT.forceEvent += ' lazyloadall';
+    $.lazyLoadXT.autoLoad = 50;
+
+    $(document).ready(function () {
+        setTimeout(function () {
+            $(window).trigger('lazyloadall');
+        }, $.lazyLoadXT.autoLoad);
+    });
+
+}(window.jQuery || window.Zepto));

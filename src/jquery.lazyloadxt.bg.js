0635d2eb9e238182ad3462a2abdb19fc09e69687
Denis Ryabov <denis@mobilejoomla.com>
2013-12-10 10:48:58 +0400

    example for background images

---
+++ src/jquery.lazyloadxt.bg.js
@@ -0,0 +1,16 @@
+/*jslint browser:true */
+/*jshint browser:true, jquery:true */
+
+(function ($) {
+    'use strict';
+
+    $.lazyLoadXT.selector += ',[data-bg]';
+
+    $(document).on('lazyshow', function () {
+        var $this = $(this);
+        $this
+            .css('background-image', "url('" + $this.attr('data-bg') + "')")
+            .removeAttr('data-bg');
+    });
+
+}(window.jQuery || window.Zepto));

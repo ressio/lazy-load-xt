9efb9095ee01c8caf8c8596c777b61d1806d604c
Denis Ryabov <denis@mobilejoomla.com>
2013-12-11 21:21:12 +0400

    srcset polyfill exaple

---
+++ src/jquery.lazyloadxt.srcset.js
@@ -0,0 +1,83 @@
+/*jslint browser:true */
+/*jshint browser:true, jquery:true */
+
+(function ($) {
+    'use strict';
+
+    var reUrl = /^\s*([^\s]*)/,
+        reWidth = /\s(\d+)w/,
+        reHeight = /\s(\d+)h/,
+        reDpr = /\s([\d\.]+)x/,
+        infty = [0, Infinity],
+        one = [0, 1];
+
+    function max(array, field) {
+        return Math.max.apply(null, $.map(array, function (item) {
+            return item[field];
+        }));
+    }
+
+    function min(array, field) {
+        return Math.min.apply(null, $.map(array, function (item) {
+            return item[field];
+        }));
+    }
+
+    $(document).on('lazyshow', function () {
+        var $this = $(this),
+            srcset = $this.attr('data-srcset'),
+            viewport = {
+                width: window.innerWidth || document.documentElement.clientWidth,
+                height: window.innerHeight || document.documentElement.clientHeight,
+                dpr: window.devicePixelRatio || 1
+            },
+            list,
+            limit;
+
+        if (!srcset) {
+            return;
+        }
+
+        list = srcset.split(',').map(function (item) {
+            return {
+                url: reUrl.exec(item)[1],
+                width: (reWidth.exec(item) || infty)[1],
+                height: (reHeight.exec(item) || infty)[1],
+                dpr: (reDpr.exec(item) || one)[1]
+            };
+        });
+
+        if (!list.length) {
+            return;
+        }
+
+        limit = max(list, 'width');
+        list = $.grep(list, function (item) {
+            return item.width >= viewport.width || item.width === limit;
+        });
+        limit = max(list, 'height');
+        list = $.grep(list, function (item) {
+            return item.height >= viewport.height || item.height === limit;
+        });
+        limit = max(list, 'dpr');
+        list = $.grep(list, function (item) {
+            return item.dpr >= viewport.dpr || item.dpr === limit;
+        });
+
+        limit = min(list, 'width');
+        list = $.grep(list, function (item) {
+            return item.width === limit;
+        });
+        limit = min(list, 'height');
+        list = $.grep(list, function (item) {
+            return item.height === limit;
+        });
+        limit = min(list, 'dpr');
+        list = $.grep(list, function (item) {
+            return item.dpr === limit;
+        });
+
+        $this.attr('src', list[0].url);
+    });
+
+}(window.jQuery || window.Zepto));

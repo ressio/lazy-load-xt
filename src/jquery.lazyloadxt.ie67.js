c16dc52c360d75c777ad43a89c6b94abd19c4ca9
Denis Ryabov <denis@mobilejoomla.com>
2013-12-12 14:23:06 +0400

    Missing radix parameter

--- src/jquery.lazyloadxt.ie67.js
+++ src/jquery.lazyloadxt.ie67.js
@@ -4,7 +4,7 @@
 (function ($) {
     'use strict';
 
-    if (parseInt(navigator.userAgent.toLowerCase().split('msie')[1] || 8) < 8) {
+    if (parseInt(navigator.userAgent.toLowerCase().split('msie')[1] || 8, 10) < 8) {
         $.lazyLoadXT.blankImage = '//upload.wikimedia.org/wikipedia/en/d/d0/Clear.gif';
     }
 

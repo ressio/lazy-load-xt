9b36b0c5187fa19a522bc09b09c2dbf3ca6688bf
Denis Ryabov <denis@mobilejoomla.com>
2013-12-13 10:51:14 +0400

    update jquery.lazyloadxt.script.js to support videos

--- src/jquery.lazyloadxt.script.js
+++ src/jquery.lazyloadxt.script.js
@@ -1,40 +1,49 @@
 /*jslint browser:true */
 /*jshint browser:true, jquery:true */
+/*jshint -W060:false */ /* we use document.write */
 
-function Z() {
-    document.write('<br data-lazy ');
-}
+(function ($, window, document) {
 
-(function ($) {
+    var dataLazyTag = $.lazyLoadXT.dataLazyTag || 'data-lazy-tag';
+
+    window.Z = function (tag, inner) {
+        document.write((inner ? '<div ' : '<br ') + dataLazyTag + '="' + (tag || 'img') + '" ');
+    };
 
     $(document).ready(function () {
         var srcAttr = $.lazyLoadXT.srcAttr;
 
-        $('br[data-lazy]').each(function () {
+        $('br[' + dataLazyTag + '],div[' + dataLazyTag + ']').each(function () {
             var attrs = this.attributes,
-                length = attrs.length,
-                img = document.createElement('img');
+                el = document.createElement($(this).attr(dataLazyTag)),
+                i;
 
-            for (var i = 0; i < length; i++) {
+            for (i = 0; i < attrs.length; i++) {
                 var attr = attrs[i];
                 if (attr.specified) {
                     var attrName = attr.nodeName,
                         attrValue = attr.nodeValue;
-                    // todo: check for '<img' attribute name
                     if (!!attrValue) {
                         if (attrName === 'src') {
-                            img.setAttribute(srcAttr, attrValue);
+                            el.setAttribute(srcAttr, attrValue);
                         } else {
-                            img.setAttribute(attrName, attrValue);
+                            el.setAttribute(attrName, attrValue);
                         }
                     }
                 }
             }
 
-            this.parentNode.replaceChild(img, this);
+            var childNodes = this.childNodes;
+            if (childNodes) {
+                for (i = 0; i < childNodes.length; i++) {
+                    el.appendChild(childNodes[i]);
+                }
+            }
+
+            this.parentNode.replaceChild(el, this);
         });
 
-        $('img[' + srcAttr + ']').lazyLoadXT();
+        $(window).lazyLoadXT();
     });
 
-}(window.jQuery || window.Zepto));
+}(window.jQuery || window.Zepto, window, document));

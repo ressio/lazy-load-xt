8ae1a50519563a1438132a43e6abc655bac91fb9
Denis Ryabov <denis@mobilejoomla.com>
2013-12-16 04:28:23 +0400

    test of addons loading

---
+++ test/lazyloadxt_addons_test.js
@@ -0,0 +1,38 @@
+/*jslint browser:true */
+/*jshint browser:true, jquery:true */
+
+(function ($) {
+    /*
+     ======== A Handy Little QUnit Reference ========
+     http://api.qunitjs.com/
+
+     Test methods:
+     module(name, {[setup][ ,teardown]})
+     test(name, callback)
+     expect(numberOfAssertions)
+     stop(increment)
+     start(decrement)
+     Test assertions:
+     ok(value, [message])
+     equal(actual, expected, [message])
+     notEqual(actual, expected, [message])
+     deepEqual(actual, expected, [message])
+     notDeepEqual(actual, expected, [message])
+     strictEqual(actual, expected, [message])
+     notStrictEqual(actual, expected, [message])
+     throws(block, [expected], [message])
+     */
+
+    module('jQuery#lazyloadxt', {
+        // This will run before each test in this module.
+        setup: function () {
+            this.elems = $('#qunit-fixture');
+        }
+    });
+
+    test('is chainable', function () {
+        expect(1);
+        strictEqual(this.elems.lazyLoadXT(), this.elems, 'should be chainable');
+    });
+
+}(window.jQuery || window.Zepto));

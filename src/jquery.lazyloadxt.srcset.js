bb2730a66dd125791f82cd35ec459dafe3d270b1
Denis Ryabov <denis@mobilejoomla.com>
2014-11-19 14:29:51 +0300

    fix work in IE6-8

--- src/jquery.lazyloadxt.srcset.js
+++ src/jquery.lazyloadxt.srcset.js
@@ -56,7 +56,7 @@
             return false;
         }
 
-        var list = srcset.split(',').map(function (item) {
+        var list = $.map(srcset.split(','), function (item) {
             return {
                 url: reUrl.exec(item)[1],
                 w: parseFloat((reWidth.exec(item) || infty)[1]),

590ac72f4d5371186aaf59624d86a878330e342d
Denis Ryabov <dryabov@yandex.ru>
2016-01-12 01:08:38 +0300

    update grunt packages, use jit-grunt to build

--- Gruntfile.js
+++ Gruntfile.js
@@ -211,14 +211,15 @@ module.exports = function (grunt) {
     });
 
     // These plugins provide necessary tasks.
-    grunt.loadNpmTasks('grunt-contrib-jshint');
+/*  grunt.loadNpmTasks('grunt-contrib-jshint');
     grunt.loadNpmTasks('grunt-contrib-clean');
     grunt.loadNpmTasks('grunt-contrib-copy');
     grunt.loadNpmTasks('grunt-contrib-concat');
     grunt.loadNpmTasks('grunt-contrib-uglify');
     grunt.loadNpmTasks('grunt-contrib-cssmin');
     grunt.loadNpmTasks('grunt-contrib-qunit');
-    grunt.loadNpmTasks('grunt-contrib-watch');
+    grunt.loadNpmTasks('grunt-contrib-watch'); */
+    require('jit-grunt')(grunt);
 
     // Default task.
     grunt.registerTask('default', ['jshint', 'clean', 'copy', 'concat', 'uglify', 'cssmin', 'qunit']);
